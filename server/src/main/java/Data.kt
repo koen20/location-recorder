d1dd184b76b58a1c9cabe874332be76642090e8c
Koen <koenhabets1@gmail.com>
2021-07-14 18:21:11 +0200

    Add timeline and map to vue. Update dependencies

--- server/src/main/java/Data.kt
+++ server/src/main/java/Data.kt
@@ -9,74 +9,76 @@ import java.lang.Exception
 import java.text.SimpleDateFormat
 
 fun Route.data(mysql: Mysql, configItem: ConfigItem) {
-    route("/info") {
-        get {
-            val startTime = call.parameters["startTime"] ?: return@get call.respondText(
-                "Missing startTime",
-                status = HttpStatusCode.BadRequest
-            )
-            val endTime = call.parameters["endTime"] ?: return@get call.respondText(
-                "Missing endTime",
-                status = HttpStatusCode.BadRequest
-            )
-            call.respondText(Gson().toJson(mysql.locationDataDao.getData(startTime.toLong(), endTime.toLong())))
-        }
-    }
-
-    route("/timeline") {
-        get {
-            val df = SimpleDateFormat("yyyy-MM-dd")
-            val date = call.parameters["date"] ?: return@get call.respondText(
-                "Missing date",
-                status = HttpStatusCode.BadRequest
-            )
-            try {
-                val timeline = Timeline(configItem, mysql)
-                call.respondText(timeline.getDataDate(df.parse(date)))
-            } catch (e: Exception) {
-                e.printStackTrace()
+    route("/api") {
+        route("/info") {
+            get {
+                val startTime = call.parameters["startTime"] ?: return@get call.respondText(
+                    "Missing startTime",
+                    status = HttpStatusCode.BadRequest
+                )
+                val endTime = call.parameters["endTime"] ?: return@get call.respondText(
+                    "Missing endTime",
+                    status = HttpStatusCode.BadRequest
+                )
+                call.respondText(Gson().toJson(mysql.locationDataDao.getData(startTime.toLong(), endTime.toLong())))
             }
         }
-    }
-    route("/timelineDb") {
-        get {
-            val df = SimpleDateFormat("yyyy-MM-dd")
-            val date = call.parameters["date"] ?: return@get call.respondText(
-                "Missing date",
-                status = HttpStatusCode.BadRequest
-            )
-            try {
-                val timeline = Timeline(configItem, mysql)
-                val dt = df.parse(date)
-                val jsonArray = JSONArray();
-                /*mysql.getLocations(dt.time / 1000, (dt.time + 86400000) / 1000).forEach {
+
+        route("/timeline") {
+            get {
+                val df = SimpleDateFormat("yyyy-MM-dd")
+                val date = call.parameters["date"] ?: return@get call.respondText(
+                    "Missing date",
+                    status = HttpStatusCode.BadRequest
+                )
+                try {
+                    val timeline = Timeline(configItem, mysql)
+                    call.respondText(timeline.getDataDate(df.parse(date)))
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        route("/timelineDb") {
+            get {
+                val df = SimpleDateFormat("yyyy-MM-dd")
+                val date = call.parameters["date"] ?: return@get call.respondText(
+                    "Missing date",
+                    status = HttpStatusCode.BadRequest
+                )
+                try {
+                    val timeline = Timeline(configItem, mysql)
+                    val dt = df.parse(date)
+                    val jsonArray = JSONArray();
+                    /*mysql.getLocations(dt.time / 1000, (dt.time + 86400000) / 1000).forEach {
                     jsonArray.put(timeline.add(it.lat, it.lon, 1, it.startDate, it.endDate))
                 }*/
-                val jsonObject = JSONObject()
-                jsonObject.put("stops", jsonArray)
-                jsonObject.put("routes", JSONArray())
-                call.respondText(jsonObject.toString())
-            } catch (e: Exception) {
-                e.printStackTrace()
+                    val jsonObject = JSONObject()
+                    jsonObject.put("stops", jsonArray)
+                    jsonObject.put("routes", JSONArray())
+                    call.respondText(jsonObject.toString())
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
         }
-    }
 
-    route("/stop") {
-        get {
-            if (call.parameters["name"] != null) {
+        route("/stop") {
+            get {
+                if (call.parameters["name"] != null) {
                     call.respondText(Gson().toJson(mysql.locationDao.getLocations(call.parameters["name"]!!)))
+                }
+                call.respondText(Gson().toJson(mysql.stopDao.getStops()))
             }
-            call.respondText(Gson().toJson(mysql.stopDao.getStops()))
-        }
-        post {
-            if (!mysql.stopDao.updateCustomName(call.parameters["stopId"]!!.toInt(), call.parameters["name"]!!)) {
-                call.respondText("Insert failed", status = HttpStatusCode.InternalServerError)
-            } else {
-                call.respondText("Added")
+            post {
+                if (!mysql.stopDao.updateCustomName(call.parameters["stopId"]!!.toInt(), call.parameters["name"]!!)) {
+                    call.respondText("Insert failed", status = HttpStatusCode.InternalServerError)
+                } else {
+                    call.respondText("Added")
+                }
             }
         }
-    }
 
-    addPhoneTrackLocation(mysql)
+        addPhoneTrackLocation(mysql)
+    }
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/stops">Stops</router-link>
   </div>
   <router-view/>
 </template>
@@ -26,5 +26,27 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#app {
+  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
+  height: 98vh;
+  width: 98vw;
+  margin: 0;
+  display: grid;
+  grid-template-rows: auto 1fr;
+}
+
+.route-icon {
+  height: 22px;
+  margin-right: 5px;
+}
+
+.stop {
+  background-color: #d7d7d7;
+}
+
+.button-add-stop{
+  margin-left: 2px;
 }
 </style>
