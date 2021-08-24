7ba5d2a12cc5d902dcc8941b36b0399ccd4528af
Koen <koenhabets1@gmail.com>
2021-08-24 21:17:56 +0200

    Db improvement

--- server/src/main/java/PhoneTrack.kt
+++ server/src/main/java/PhoneTrack.kt
@@ -2,32 +2,33 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import java.lang.Exception
+import java.sql.PreparedStatement
 import java.sql.SQLException
+import java.sql.Types
 
-fun Route.addPhoneTrackLocation(mysql: Mysql){
+fun Route.addPhoneTrackLocation(mysql: Mysql) {
     route("/add") {
         get {
             try {
                 val tst = Mqtt.getMysqlDateString(call.parameters["timestamp"]!!.toLong())
-                val stmt = mysql.conn.createStatement()
                 val alt = call.parameters["alt"]!!
+
+                val ps: PreparedStatement =
+                    mysql.conn.prepareStatement("INSERT INTO data VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)")
+                ps.setString(1, tst)
+                ps.setDouble(2, call.parameters["lat"]!!.toDouble())
+                ps.setDouble(3, call.parameters["lon"]!!.toDouble())
                 if (alt == "") {
-                    stmt.executeUpdate(
-                        "INSERT INTO data VALUES (NULL, '" + tst + "', '"
-                                + call.parameters["lat"]!! + "', '" + call.parameters["lon"]!! + "'" +
-                                ", NULL, '" + call.parameters["acc"]!!
-                                + "',  '" + call.parameters["batt"]!! + "', '" + call.parameters["tid"]!! + "')"
-                    )
+                    ps.setNull(4, Types.DOUBLE)
                 } else {
-                    stmt.executeUpdate(
-                        ("INSERT INTO data VALUES (NULL, '" + tst + "', '"
-                                + call.parameters["lat"]!! + "', '" + call.parameters["lon"]!! + "'" +
-                                ", '" + alt + "', '" + call.parameters["acc"]!!
-                                + "',  '" + call.parameters["batt"]!! + "', '" + call.parameters["tid"]!! + "')")
-                    )
+                    ps.setDouble(4, alt.toDouble())
                 }
-                stmt.close()
+
+                ps.setString(5, call.parameters["acc"]!!)
+                ps.setString(6, call.parameters["batt"]!!)
+                ps.setString(7, call.parameters["tid"]!!)
+                ps.execute()
+
                 call.respondText("added")
             } catch (e: Exception) {
                 e.printStackTrace()
